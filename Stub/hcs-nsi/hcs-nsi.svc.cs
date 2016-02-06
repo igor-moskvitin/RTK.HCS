98647a23bc314ad165ee2d85410e0b9f0e3fc448
mig <mig84@yandex.ru>
2016-02-06 23:52:47 +0600

    some changes

--- Stub/hcs-nsi/hcs-nsi.svc.cs
+++ Stub/hcs-nsi/hcs-nsi.svc.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using System.Threading.Tasks;
 using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
 
 namespace Stub.hcs_nsi
 {
@@ -13,9 +15,60 @@ namespace Stub.hcs_nsi
     public class Nsi : INsi
     {
         Fixture fixture = new Fixture();
+
+        public Nsi()
+        {
+            //fixture.Customizations.Add(new AutoMoqCustomization());
+        }
+
         public exportNsiListResponse exportNsiList(exportNsiListRequest1 request)
         {
             return fixture.Create<exportNsiListResponse>();
+        }
+
+        public Task<exportNsiListResponse> exportNsiListAsync(exportNsiListRequest1 request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public exportNsiItemResponse exportNsiItem(exportNsiItemRequest1 request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<exportNsiItemResponse> exportNsiItemAsync(exportNsiItemRequest1 request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public importAdditionalServicesResponse importAdditionalServices(importAdditionalServicesRequest1 request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<importAdditionalServicesResponse> importAdditionalServicesAsync(importAdditionalServicesRequest1 request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public importMunicipalServicesResponse importMunicipalServices(importMunicipalServicesRequest1 request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<importMunicipalServicesResponse> importMunicipalServicesAsync(importMunicipalServicesRequest1 request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public importOrganizationWorksResponse importOrganizationWorks(importOrganizationWorksRequest1 request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<importOrganizationWorksResponse> importOrganizationWorksAsync(importOrganizationWorksRequest1 request)
+        {
+            throw new NotImplementedException();
         }
     }
 }
